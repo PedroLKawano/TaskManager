8c7c8c3aeb066fe3d7af943bcf18257d432da030
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-01-13 14:11:06 -0300

    improve: adds task entity

---
+++ TaskManager.Domain/Entities/Task.cs
@@ -0,0 +1,16 @@
+﻿using TaskManager.Domain.Enums;
+
+namespace TaskManager.Domain.Entities
+{
+    public class Task
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; } = string.Empty;
+        public string Description { get; private set; } = string.Empty;
+        public string Category { get; private set; } = string.Empty;
+        public DateTime CreationDate { get; set; }
+        public DateTime? ConclusionDate { get; private set; }
+        public Status Status { get; private set; }
+        public Priority Priority { get; private set; }
+    }
+}
---
+++ TaskManager.Domain/Enums/Priority.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Domain.Enums
+{
+    public enum Priority
+    {
+        Low = 0,
+        Normal = 1,
+        High = 2,
+        Urgent = 3
+    }
+}
