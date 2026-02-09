b412e17b6e1a9f1486b624993b1d71d18571f2b5
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-09 14:02:27 -0300

    improve: creates category repository

---
+++ TaskManager.Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskManager.Domain.Entities;
+
+namespace TaskManager.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        void Add(Category category);
+        void Update(Category category);
+        void Remove(Guid id);
+        Category GetById(Guid id);
+        IEnumerable<Category> GetAll();
+    }
+}
