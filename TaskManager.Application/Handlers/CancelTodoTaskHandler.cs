8cb7481f449f5949ccb56fd94360c9f23a28c179
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-09 16:47:10 -0300

    improve: adds cancel todo task command and handler

---
+++ TaskManager.Application/Handlers/CancelTodoTaskHandler.cs
@@ -0,0 +1,21 @@
+﻿using TaskManager.Domain.Commands;
+using TaskManager.Domain.Exceptions;
+using TaskManager.Domain.Repositories;
+
+namespace TaskManager.Application.Handlers
+{
+    public class CancelTodoTaskHandler(ITodoTaskRepository repository)
+    {
+        private readonly ITodoTaskRepository _repository = repository;
+
+        public void Handle(CancelTodoTaskCommand command)
+        {
+            var task = _repository.GetById(command.TodoTaskId) ??
+                throw new DomainException("Tarefa não encontrada.");
+
+            task.Cancel();
+
+            _repository.Update(task);
+        }
+    }
+}
