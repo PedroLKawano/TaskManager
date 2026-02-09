4d0bf58da87e33189ac871c217dd809d67fa4331
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-09 14:02:40 -0300

    improve: creates todotask repository

---
+++ TaskManager.Domain/Repositories/ITodoTaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaskManager.Domain.Entities;
+using TaskManager.Domain.Enums;
+
+namespace TaskManager.Domain.Repositories
+{
+    public interface ITodoTaskRepository
+    {
+        void Add(TodoTask task);
+        void Update(TodoTask task);
+        void Remove(Guid id);
+        TodoTask GetById(Guid id);
+        IEnumerable<TodoTask> GetByStatus(Status status);
+    }
+}
