4a2b3d113e9489bbc85c70022d621d6cc314be40
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-09 14:35:55 -0300

    improve: adds createtodotaskhandler

---
+++ TaskManager.Application/Handlers/CreateTodoTaskHandler.cs
@@ -0,0 +1,27 @@
+﻿using TaskManager.Domain.Commands;
+using TaskManager.Domain.Entities;
+using TaskManager.Domain.Exceptions;
+using TaskManager.Domain.Repositories;
+
+namespace TaskManager.Application.Handlers
+{
+    public class CreateTodoTaskHandler(ITodoTaskRepository taskRepository,
+                                       ICategoryRepository categoryRepository)
+    {
+        private readonly ITodoTaskRepository _taskRepository = taskRepository;
+        private readonly ICategoryRepository _categoryRepository = categoryRepository;
+
+        public void Handle(CreateTodoTaskCommand command)
+        {
+            var category = _categoryRepository.GetById(command.CategoryId) ??
+                throw new DomainException("Categoria da tarefa não encontrada.");
+            
+            var task = new TodoTask(command.Title,
+                                    command.Priority,
+                                    command.Description,
+                                    category);
+
+            _taskRepository.Add(task);
+        }
+    }
+}
