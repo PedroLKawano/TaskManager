58e5c992ae5eb433e60936c6acb339909e5deb1a
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-09 16:14:04 -0300

    improve: creates start todo task command and handler

---
+++ TaskManager.Application/Handlers/StartTodoTaskHandler.cs
@@ -0,0 +1,21 @@
+﻿using TaskManager.Domain.Commands;
+using TaskManager.Domain.Exceptions;
+using TaskManager.Domain.Repositories;
+
+namespace TaskManager.Application.Handlers
+{
+    public class StartTodoTaskHandler(ITodoTaskRepository repository)
+    {
+        private readonly ITodoTaskRepository _repository = repository;
+
+        public void Handle(StartTodoTaskCommand command)
+        {
+            var task = _repository.GetById(command.TodoTaskId) ??
+                throw new DomainException("Tarefa não encontrada.");
+            
+            task.Start();
+
+            _repository.Update(task);
+        }
+    }
+}
