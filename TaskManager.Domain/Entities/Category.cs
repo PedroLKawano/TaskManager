e2e35569c01305a03dea9662e6a0ffb6a3fb07ca
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-09 13:45:24 -0300

    improve: improves category

--- TaskManager.Domain/Entities/Category.cs
+++ TaskManager.Domain/Entities/Category.cs
@@ -1,9 +1,30 @@
-﻿namespace TaskManager.Domain.Entities
+﻿using TaskManager.Domain.Exceptions;
+
+namespace TaskManager.Domain.Entities
 {
     public class Category
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; } = string.Empty;
-        public string Description { get; private set; } = string.Empty;
+        public Category(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new DomainException("O nome é obrigatório.");
+
+            Name = name;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string? Description { get; private set; }
+
+        public void SetDescription(string description)
+        {
+            if (string.IsNullOrWhiteSpace(description))
+                throw new DomainException("A descrição não pode estar vazia");
+
+            if (description.Length > 150)
+                throw new DomainException("A descrição pode conter no máximo 150 caracteres");
+
+            Description = description;
+        }
     }
 }
