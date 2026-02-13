b48deab23c2b0f137c7cabd2a38c422123903a06
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-13 17:42:43 -0300

    improve: adds category configuration

---
+++ TaskManager.Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Infrastructure.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+            
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(150);
+        }
+    }
+}
