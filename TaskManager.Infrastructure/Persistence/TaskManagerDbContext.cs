711fea5c2e772e7dacd04ad5db3df64866a217e1
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-13 17:04:04 -0300

    improve: adds task manager db context

---
+++ TaskManager.Infrastructure/Persistence/TaskManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Infrastructure.Persistence
+{
+    public class TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options) : DbContext(options)
+    {
+        public DbSet<TodoTask> TodoTasks { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TaskManagerDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
