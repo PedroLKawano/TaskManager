e1b62c50ceb1c5718cd69ec15da85dbc52852d3c
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-18 17:33:29 -0300

    improve: implements dependency injection

---
+++ TaskManager.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskManager.Domain.Repositories;
+using TaskManager.Domain.UnitOfWork;
+using TaskManager.Infrastructure.Persistence;
+using TaskManager.Infrastructure.Repositories;
+
+namespace TaskManager.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+                                                       IConfiguration configuration)
+    {
+        services.AddDbContext<TaskManagerDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<ITodoTaskRepository, TodoTaskRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IUnitOfWork>(provider =>
+            provider.GetRequiredService<TaskManagerDbContext>());
+
+        return services;
+    }
+}
