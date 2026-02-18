e413540e2c0751587ab6095cb8fbf8742617af9e
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-18 17:30:36 -0300

    improve: implements UnitOfWork

--- TaskManager.Infrastructure/Persistence/TaskManagerDbContext.cs
+++ TaskManager.Infrastructure/Persistence/TaskManagerDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TaskManager.Domain.Entities;
+using TaskManager.Domain.UnitOfWork;
 
 namespace TaskManager.Infrastructure.Persistence;
 
-public class TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options) : DbContext(options)
+public class TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options) :
+    DbContext(options), IUnitOfWork
 {
     public DbSet<TodoTask> TodoTasks { get; set; } = null!;
     public DbSet<Category> Categories { get; set; } = null!;
