6c0eb112964c91239869ff25113ed69e36b7cfaa
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-18 15:16:04 -0300

    improve: adds TodoTaskConfiguration

---
+++ TaskManager.Infrastructure/Persistence/Configurations/TodoTaskConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Infrastructure.Persistence.Configurations;
+
+public class TodoTaskConfiguration : IEntityTypeConfiguration<TodoTask>
+{
+    public void Configure(EntityTypeBuilder<TodoTask> builder)
+    {
+        builder.ToTable("TodoTasks");
+
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(t => t.Title)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(t => t.Description)
+            .HasMaxLength(150);
+
+        builder.Property(t => t.CategoryId)
+            .IsRequired();
+
+        builder.HasOne(t => t.Category)
+            .WithMany(c => c.TodoTasks)
+            .HasForeignKey(t => t.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Property(t => t.CreationDate)
+            .IsRequired();
+
+        builder.Property(t => t.ConclusionDate)
+            .IsRequired(false);
+
+        builder.Property(t => t.Status)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.Property(t => t.Priority)
+            .HasConversion<int>()
+            .IsRequired();
+    }
+}
