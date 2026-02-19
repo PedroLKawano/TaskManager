5de599c983638c1b46a56beb1ee6edfc354630ed
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-19 16:39:27 -0300

    improve: adds DependencyInjection in application

---
+++ TaskManager.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TaskManager.Application.Handlers;
+
+namespace TaskManager.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<CreateTodoTaskHandler>();
+        services.AddScoped<StartTodoTaskHandler>();
+        services.AddScoped<CompleteTodoTaskHandler>();
+        services.AddScoped<CancelTodoTaskHandler>();
+        services.AddScoped<CreateCategoryHandler>();
+
+        return services;
+    }
+}
