01204f6623c01b9917443f73b0419e0a5f4895e4
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-19 16:39:50 -0300

    improve: adds controllers to the api project

---
+++ TaskManager.API/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Application.Handlers;
+using TaskManager.Domain.Commands;
+
+namespace TaskManager.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController(CreateCategoryHandler handler) : ControllerBase
+{
+    private readonly CreateCategoryHandler _handler = handler;
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateCategoryCommand command,
+                                            CancellationToken cancellationToken)
+    {
+        var id = await _handler.HandleAsync(command, cancellationToken);
+
+        return CreatedAtAction(nameof(GetById), new { id }, id);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetById(Guid id)
+    {
+        return Ok();
+    }
+}
---
+++ TaskManager.API/Controllers/TodoTaskController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Application.Handlers;
+using TaskManager.Domain.Commands;
+
+namespace TaskManager.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TodoTaskController(CreateTodoTaskHandler createHandler,
+                                StartTodoTaskHandler startHandler,
+                                CompleteTodoTaskHandler completeHandler,
+                                CancelTodoTaskHandler cancelHandler) : ControllerBase
+{
+    private readonly CreateTodoTaskHandler _createHandler = createHandler;
+    private readonly StartTodoTaskHandler _startHandler = startHandler;
+    private readonly CompleteTodoTaskHandler _completeHandler = completeHandler;
+    private readonly CancelTodoTaskHandler _cancelHandler = cancelHandler;
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateTodoTaskCommand command,
+                                            CancellationToken cancellationToken)
+    {
+        var id = await _createHandler.HandleAsync(command, cancellationToken);
+
+        return CreatedAtAction(nameof(GetById), new { id }, id);
+    }
+
+    [HttpPost("{id:guid}/start")]
+    public async Task<IActionResult> Start(Guid id, CancellationToken cancellationToken)
+    {
+        await _startHandler.HandleAsync(new StartTodoTaskCommand(id), cancellationToken);
+        return NoContent();
+    }
+
+    [HttpPost("{id:guid}/complete")]
+    public async Task<IActionResult> Complete(Guid id, CancellationToken cancellationToken)
+    {
+        await _completeHandler.HandleAsync(new CompleteTodoTaskCommand(id), cancellationToken);
+        return NoContent();
+    }
+
+    [HttpPost("{id:guid}/cancel")]
+    public async Task<IActionResult> Cancel(Guid id, CancellationToken cancellationToken)
+    {
+        await _cancelHandler.HandleAsync(new CancelTodoTaskCommand(id), cancellationToken);
+        return NoContent();
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetById(Guid id)
+    {
+        return Ok();
+    }
+}
