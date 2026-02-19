0794db21b7ae0579f27b3e92fc80be290e4bc215
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-19 12:20:14 -0300

    improve: updates CreateCategoryHandler to use async methods

--- TaskManager.Application/Handlers/CreateCategoryHandler.cs
+++ TaskManager.Application/Handlers/CreateCategoryHandler.cs
@@ -1,19 +1,25 @@
-﻿using TaskManager.Domain.Commands;
+﻿using TaskManager.Domain.Abstractions;
+using TaskManager.Domain.Commands;
 using TaskManager.Domain.Entities;
 using TaskManager.Domain.Repositories;
 
 namespace TaskManager.Application.Handlers;
 
-public class CreateCategoryHandler(ICategoryRepository repository)
+public class CreateCategoryHandler(ICategoryRepository repository, IUnitOfWork unitOfWork)
 {
     private readonly ICategoryRepository _repository = repository;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
-    public void Handle(CreateCategoryCommand command)
+    public async Task<Guid> HandleAsync(CreateCategoryCommand command,
+                                        CancellationToken cancellationToken = default)
     {
         var category = new Category(command.Name);
 
         category.SetDescription(command.Description);
 
-        _repository.Add(category);
+        await _repository.AddAsync(category, cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return category.Id;
     }
 }
