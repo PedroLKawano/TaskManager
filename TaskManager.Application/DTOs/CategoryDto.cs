f7bbea207b9d49f199344b0e8a3dfe4674295dd5
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-19 16:49:22 -0300

    improve: creates dtos

---
+++ TaskManager.Application/DTOs/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Application.DTOs;
+
+public class CategoryDto
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string? Description { get; init; }
+}
---
+++ TaskManager.Application/DTOs/TodoTaskDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.Application.DTOs;
+
+public class TodoTaskDto
+{
+    public Guid Id { get; init; }
+    public string Title { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+    public string CategoryName { get; init; } = string.Empty;
+    public string Status { get; init; } = string.Empty;
+    public string Priority { get; init; } = string.Empty;
+    public DateTime CreationDate { get; init; }
+    public DateTime? ConclusionDate { get; init; }
+}
