d40fc71344cfca2b17c184f72c4d3579822b82d6
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-20 16:55:56 -0300

    improve: implements GetCategoryByIdQueryHandler

---
+++ TaskManager.Application/Queries/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using TaskManager.Application.DTOs;
+using TaskManager.Domain.Repositories;
+
+namespace TaskManager.Application.Queries;
+
+public class GetCategoryByIdQueryHandler(ICategoryRepository repository)
+{
+    private readonly ICategoryRepository _repository = repository;
+
+    public async Task<CategoryDto?> HandleAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var category = await _repository.GetByIdAsync(id, cancellationToken);
+
+        if (category is null) return null;
+
+        return new CategoryDto
+        {
+            Id = category.Id,
+            Name = category.Name,
+            Description = category.Description
+        };
+    }
+}
