5cf749515003670d5b8c69fd5ac032c33bb441ae
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-20 17:07:18 -0300

    improve: implementa GetAllCategoriesQueryHandler

---
+++ TaskManager.Application/Queries/GetAllCategoriesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using TaskManager.Application.DTOs;
+using TaskManager.Domain.Repositories;
+
+namespace TaskManager.Application.Queries;
+
+public class GetAllCategoriesQueryHandler(ICategoryRepository repository)
+{
+    private readonly ICategoryRepository _repository = repository;
+
+    public async Task<IEnumerable<CategoryDto>> HandleAsync(CancellationToken cancellationToken = default)
+    {
+        var categories = await _repository.GetAllAsync(cancellationToken);
+
+        return categories.Select(category => new CategoryDto
+        {
+            Id = category.Id,
+            Name = category.Name,
+            Description = category.Description
+        });
+    }
+}
