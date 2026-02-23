51c2ac64187ef64036d0da5f976f56f29e14141d
Pedro Lucas Kawano <pedrolkawano@outlook.com>
2026-02-23 15:48:39 -0300

    improve: implements name update on category

--- TaskManager.Domain/Entities/Category.cs
+++ TaskManager.Domain/Entities/Category.cs
@@ -8,11 +8,7 @@ public class Category
 
     public Category(string name)
     {
-        if (string.IsNullOrWhiteSpace(name))
-            throw new DomainException("O nome é obrigatório.");
-
-        if (name.Length > 50)
-            throw new DomainException("O nome pode conter no máximo 50 caracteres.");
+        ValidateName(name);
 
         Id = Guid.NewGuid();
         Name = name;
@@ -22,12 +18,28 @@ public class Category
     public string Name { get; private set; } = null!;
     public string? Description { get; private set; }
     public ICollection<TodoTask> TodoTasks { get; private set; } = [];
-
+    
     public void SetDescription(string? description)
     {
         if (description?.Length > 150)
             throw new DomainException("A descrição pode conter no máximo 150 caracteres");
 
         Description = description;
+    }
+
+    public void UpdateName(string name)
+    {
+        ValidateName(name);
+
+        Name = name;
+    }
+
+    private static void ValidateName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new DomainException("O nome é obrigatório.");
+
+        if (name.Length > 50)
+            throw new DomainException("O nome pode conter no máximo 50 caracteres.");
     }
 }
